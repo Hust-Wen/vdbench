2fa140ef93b16bd8fe67091d47bb601960dfad39
wenyuhong <wenyuhong@hust.edu.cn>
2024-09-04 09:37:09 +0000

    修改了编译脚本和运行脚本，当安装了多个JAVA版本时，选择1.8及以下的版本进行编译和运行，否则可能出现不兼容问题

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 find -name "*.java" > sources.txt
-javac @sources.txt -d ./classes -encoding utf-8 > output.log
-jar -cvfM0 vdbench.jar ./ >> output.log
+/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/javac @sources.txt -d ./classes -encoding utf-8 > output.log
+/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/jar -cvfM0 vdbench.jar ./ >> output.log
